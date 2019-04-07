298b30eeb902e48dbf961ec8248900ad270d9bfc
eric hackborn <hackborn@gmail.com>
2019-04-07 13:46:15 -0700

    Clean up extract API (moved to path terminology). Add conveniences for replacing path items and getting two results.

--- path.go
+++ path.go
@@ -4,6 +4,92 @@ import (
 	"strings"
 )
 
+// PathOne() answers a single item from the path input, reconstructed from
+// the results. There's no error reporting, if you need more complete information
+// use a more complex form.
+func PathOne(path string, opts PathOpts, rules ...interface{}) string {
+	ext, err := Path(path, opts, rules...)
+	if err != nil || len(ext) < 1 {
+		return ""
+	}
+	if len(ext) == 1 {
+		return ext[0]
+	}
+	ans := ext[0]
+	for i := 1; i < len(ext); i++ {
+		ans += opts.Separator + ext[i]
+	}
+	return ans
+}
+
+// PathTwo() answers two items from the path input. There's no error reporting,
+// if you need more complete information use a more complex form.
+func PathTwo(path string, opts PathOpts, rules ...interface{}) (string, string) {
+	ext, _ := Path(path, opts, rules...)
+	a := ""
+	b := ""
+	if len(ext) > 0 {
+		a = ext[0]
+	}
+	if len(ext) > 1 {
+		b = ext[1]
+	}
+	return a, b
+}
+
+// func Path() takes a list of strings and returns portions of it, according to the rules.
+// Rules can contain either strings or a path rule.
+// Strings:
+//		The node must match the string exactly. It is not included in the response.
+// Path rules:
+//		KeepRule. Node can be anything. It is returned in the response.
+//		KeepAllRule. All remaining nodes are added to the response and returned.
+//		ReplaceRuleT. Replace this item with the Replace.With.
+//		SkipRule. Node can be anything. It is not included in the response.
+func Path(_path string, opts PathOpts, rules ...interface{}) ([]string, error) {
+	if opts.Separator == "" {
+		return nil, badRequestErr
+	}
+	src := strings.Split(_path, opts.Separator)
+	var resp []string
+	srci := 0
+	for _, k := range rules {
+		// Skip empties in the source
+		for srci < len(src) && src[srci] == "" {
+			srci++
+		}
+		if srci >= len(src) {
+			return nil, ruleMismatchErr
+		}
+
+		switch rule := k.(type) {
+		case string:
+			if src[srci] != rule {
+				return nil, stringMismatchErr
+			}
+		case KeepRuleT:
+			resp = append(resp, src[srci])
+		case KeepAllRuleT:
+			// This can only be the final element.
+			resp = append(resp, src[srci:len(src)]...)
+			return resp, nil
+		case ReplaceRuleT:
+			resp = append(resp, rule.With)
+		case SkipRuleT:
+		default:
+			return nil, invalidRuleErr
+		}
+		srci++
+	}
+	return resp, nil
+}
+
+type PathOpts struct {
+	Separator string
+}
+
+// ---------- DEPRECATED API
+
 // func ExtractSep() is a wrapper on Extract(), first splitting the string based on the separator.
 func ExtractSep(path, separator string, rules ...interface{}) ([]string, error) {
 	if separator == "" {
@@ -37,14 +123,15 @@ func Extract(src []string, rules ...interface{}) ([]string, error) {
 			if src[srci] != rule {
 				return nil, stringMismatchErr
 			}
-		case ExtractRule:
-			switch rule {
-			case KeepRule:
-				resp = append(resp, src[srci])
-			case SkipRule:
-			default:
-				return nil, invalidExtractRuleErr
-			}
+		case KeepRuleT:
+			resp = append(resp, src[srci])
+		case KeepAllRuleT:
+			// This can only be the final element.
+			resp = append(resp, src[srci:len(src)]...)
+			return resp, nil
+		case ReplaceRuleT:
+			resp = append(resp, rule.With)
+		case SkipRuleT:
 		default:
 			return nil, invalidRuleErr
 		}
@@ -53,27 +140,18 @@ func Extract(src []string, rules ...interface{}) ([]string, error) {
 	return resp, nil
 }
 
-// func ExtractOne() extracts a single item. There's no error reporting, if you
-// need more complete information use a more complex form.
-func ExtractOne(path string, opts ExtractOpts, rules ...interface{}) string {
-	if opts.Separator == "" {
-		return ""
-	}
-	all := strings.Split(path, opts.Separator)
-	ext, err := Extract(all, rules...)
-	if err != nil || len(ext) < 1 {
-		return ""
-	}
-	return ext[0]
+// ------------------------------------------------------------
+// RULES
+
+type KeepRuleT struct{}
+type KeepAllRuleT struct{}
+type ReplaceRuleT struct {
+	With string
 }
+type SkipRuleT struct{}
 
-type ExtractOpts struct {
-	Separator string
-}
-
-type ExtractRule int
-
-const (
-	KeepRule ExtractRule = iota // Keep the item at this location, copying it to the results
-	SkipRule
+var (
+	KeepRule    = KeepRuleT{}
+	KeepAllRule = KeepAllRuleT{}
+	SkipRule    = SkipRuleT{}
 )
--- path_test.go
+++ path_test.go
@@ -5,6 +5,69 @@ import (
 	"testing"
 )
 
+func TestPathOne(t *testing.T) {
+	cases := []struct {
+		Path  string
+		Rules []interface{}
+		Want  string
+	}{
+		{"a", []interface{}{KeepRule}, "a"},
+		{"a", []interface{}{SkipRule}, ""},
+		{"a/b", []interface{}{KeepRule}, "a"},
+		{"a/b", []interface{}{KeepRuleT{}}, "a"},
+		{"a/b", []interface{}{KeepRule, KeepRule}, "a/b"},
+		{"a/b", []interface{}{SkipRule, KeepRule}, "b"},
+		{"a/b/c", []interface{}{KeepRule, SkipRule}, "a"},
+		{"a", []interface{}{KeepAllRule}, "a"},
+		{"a/b", []interface{}{KeepAllRule}, "a/b"},
+		{"a/b/c", []interface{}{KeepAllRule}, "a/b/c"},
+		{"a/b/c", []interface{}{KeepRule, ReplaceRuleT{"E"}}, "a/E"},
+		{"a/b/c", []interface{}{KeepRule, ReplaceRuleT{"E"}, KeepRule}, "a/E/c"},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			opts := PathOpts{Separator: "/"}
+			have := PathOne(tc.Path, opts, tc.Rules...)
+			if have != tc.Want {
+				fmt.Println("Mismtach have", have, "want", tc.Want)
+				t.Fail()
+			}
+		})
+	}
+}
+
+func TestPathTwo(t *testing.T) {
+	cases := []struct {
+		Path  string
+		Rules []interface{}
+		Want  []string
+	}{
+		{"a", []interface{}{KeepRule}, []string{"a"}},
+		{"a/b", []interface{}{KeepRule}, []string{"a"}},
+		{"a/b", []interface{}{KeepAllRule}, []string{"a", "b"}},
+		{"a/b/c", []interface{}{KeepAllRule}, []string{"a", "b"}},
+		{"a/b/c", []interface{}{KeepRule, SkipRule}, []string{"a"}},
+		{"a/b/c", []interface{}{KeepRule, SkipRule, KeepRule}, []string{"a", "c"}},
+	}
+	for i, tc := range cases {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			opts := PathOpts{Separator: "/"}
+			havea, haveb := PathTwo(tc.Path, opts, tc.Rules...)
+			var have []string
+			if havea != "" {
+				have = append(have, havea)
+			}
+			if haveb != "" {
+				have = append(have, haveb)
+			}
+			if arraysMatch(tc.Want, have) == false {
+				fmt.Println("Mismtach have", have, "want", tc.Want)
+				t.Fail()
+			}
+		})
+	}
+}
+
 func TestPathExtractSepAccept(t *testing.T) {
 	cases := []struct {
 		Path  string
@@ -44,29 +107,6 @@ func TestPathExtractSepDecline(t *testing.T) {
 		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
 			have, err := ExtractSep(tc.Path, "/", tc.Rules...)
 			if err == nil && arraysMatch(tc.Want, have) == true {
-				t.Fail()
-			}
-		})
-	}
-}
-
-func TestPathExtractOne(t *testing.T) {
-	cases := []struct {
-		Path  string
-		Rules []interface{}
-		Want  string
-	}{
-		{"a", []interface{}{KeepRule}, "a"},
-		{"a", []interface{}{SkipRule}, ""},
-		{"a/b", []interface{}{KeepRule, KeepRule}, "a"},
-		{"a/b", []interface{}{SkipRule, KeepRule}, "b"},
-	}
-	for i, tc := range cases {
-		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
-			opts := ExtractOpts{Separator: "/"}
-			have := ExtractOne(tc.Path, opts, tc.Rules...)
-			if have != tc.Want {
-				fmt.Println("Mismtach have", have, "want", tc.Want)
 				t.Fail()
 			}
 		})
